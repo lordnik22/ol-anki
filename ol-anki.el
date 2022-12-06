c6b80db81e1fd42b5d3962a38606baca50f53557
lordnik22 <michi.b_98@hotmail.com>
2022-12-06 16:31:51 +0100

    Update anki-editor and prefix with ol-anki. Add header-documentation

--- ol-anki.el
+++ ol-anki.el
@@ -1,14 +1,22 @@
 ;;; ol-anki.el - Support for links to anki decks in Org mode
+
+;; Copyright (C) 2022 lordnik22
+
+;; Author: lordnik22
+;; Version: 1.0.0
+;; Keywords: extensions
+;; URL: https://github.com/lordnik22
+;; Package-Requires: ((emacs "28") (request "0.8.0"))
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;; Commentary:
+;;; Add a new org-link-type: anki. When opened, will start anki and focus the deck specified by the link.
+;;; Code:
 (require 'request)
 (require 'ol)
-
-(defcustom anki-editor-anki-connect-listening-address
-  "127.0.0.1"
-  "The network address AnkiConnect is listening.")
-
-(defcustom anki-editor-anki-connect-listening-port
-  "8765"
-  "The port number AnkiConnect is listening.")
 
 (org-link-set-parameters "anki"
 			 :follow #'ol-anki-open
@@ -25,7 +33,7 @@
   "Visit the anki deck named ‘NAME’.
 ‘NAME’ should be a deck that can exists in you anki-program."
   (shell-command "anki && xdotool windowactivate `xdotool search --pid \\`pgrep anki\\``")
-  (anki-editor--anki-connect-invoke-result "guiDeckOverview" (list (cons 'name name))))
+  (ol-anki-api-call-result "guiDeckOverview" (cons 'name name)))
 
 (defun ol-anki-store-link ()
   "Not implemented because anki is an external program"
@@ -40,57 +48,98 @@
       (`ascii (format "%s (%s)" desc link))
       (_ link)))
 
-(defun anki-editor--anki-connect-action (action &optional params version)
-  (let (a)
-    (when version
-      (push `(version . ,version) a))
-    (when params
-      (push `(params . ,params) a))
-    (push `(action . ,action) a)))
-
-(defun anki-editor--anki-connect-invoke (action &optional params)
-  "Invoke AnkiConnect with ACTION and PARAMS."
-  (let ((request-body (json-encode (anki-editor--anki-connect-action action params 5)))
-	(request-backend 'curl)
-	(json-array-type 'list)
-	reply err)
-
-    (let ((response (request (format "http://%s:%s"
-				     anki-editor-anki-connect-listening-address
-				     anki-editor-anki-connect-listening-port)
-			     :type "POST"
-			     :parser 'json-read
-			     :data request-body
-			     :success (cl-function (lambda (&key data &allow-other-keys)
-						     (setq reply data)))
-			     :error (cl-function (lambda (&key _ &key error-thrown &allow-other-keys)
-						   (setq err (string-trim (cdr error-thrown)))))
-			     :sync t)))
-
-      ;; HACK: With sync set to t, `request' waits for curl process to
-      ;; exit, then response data becomes available, but callbacks
-      ;; might not be called right away but at a later time, that's
-      ;; why here we manually invoke callbacks to receive the result.
-      (unless (request-response-done-p response)
-	(request--curl-callback (get-buffer-process (request-response--buffer response)) "finished\n")))
-
-    (when err (error "Error communicating with AnkiConnect using cURL: %s" err))
-    (or reply (error "Got empty reply from AnkiConnect"))))
-
 (defun ol-anki-complete-anki ()
   "Completion with all existing anki decks when creating anki deck links."
   (concat (symbol-name ol-anki-command) ":"
-	  (ido-completing-read "Choose Anki deck: " (anki-editor-deck-names))))
+	  (ido-completing-read "Choose Anki deck: " (ol-anki-deck-names))))
 
-(defmacro anki-editor--anki-connect-invoke-result (&rest args)
-  "Invoke AnkiConnect with ARGS, return the result from response or raise an error."
-  `(let-alist (anki-editor--anki-connect-invoke ,@args)
-     (when .error (error .error))
-     .result))
+;;; Copied from https://github.com/orgtre/anki-editor/blob/master/anki-editor.el and prefixed with ol-anki
+(defcustom ol-anki-api-host "127.0.0.1"
+  "The network address AnkiConnect is listening on."
+  :type 'string)
 
-(defun anki-editor-deck-names ()
+(defcustom ol-anki-api-port "8765"
+  "The port number AnkiConnect is listening on."
+  :type 'string)
+
+(defconst ol-anki-api-version 6)
+
+(defun ol-anki-deck-names ()
   "Get all decks names from Anki."
-  (anki-editor--anki-connect-invoke-result "deckNames"))
+  (ol-anki-api-call-result "deckNames"))
+
+(cl-defun ol-anki--fetch (url
+			      &rest settings
+			      &key
+			      (type "GET")
+			      data success _error
+			      (parser 'buffer-string)
+			      &allow-other-keys)
+  "Fetch URL using curl.
+The api is borrowed from request.el."
+;; This exists because request.el's sync mode calls curl asynchronously under
+;; the hood, which doesn't work on some machines (like mine) where the process
+;; sentinel never gets called. After some debugging of Emacs, it seems that in
+;; 'process.c' the pselect syscall to the file descriptor of inotify used by
+;; 'autorevert' always returns a nonzero value and causes 'status_notify' never
+;; being called. To determine whether it's a bug in Emacs and make a patch
+;; requires more digging.
+  (let ((tempfile (make-temp-file "emacs-ol-anki"))
+	(responsebuf (generate-new-buffer " *ol-anki-curl*")))
+    (when data
+      (with-temp-file tempfile
+	(setq buffer-file-coding-system 'utf-8)
+	(set-buffer-multibyte t)
+	(insert data)))
+    (unwind-protect
+	(with-current-buffer responsebuf
+	  (apply #'call-process "curl" nil t nil (list
+						  url
+						  "--silent"
+						  "-X" type
+						  "--data-binary"
+						  (concat "@" tempfile)))
+
+	  (goto-char (point-min))
+	  (when success
+	    (apply success (list :data (funcall parser)))))
+      (kill-buffer responsebuf)
+      (delete-file tempfile))))
+
+(defun ol-anki-api-call (action &rest params)
+  "Invoke AnkiConnect with ACTION and PARAMS."
+  (let ((payload (list :action action :version ol-anki-api-version))
+	(_request-backend 'curl)
+	(json-array-type 'list)
+	reply err)
+
+    (when params
+      (plist-put payload :params params))
+
+    (ol-anki--fetch (format "http://%s:%s"
+				ol-anki-api-host
+				ol-anki-api-port)
+			:type "POST"
+			:parser 'json-read
+			:data (json-encode payload)
+			:success (cl-function
+				  (lambda (&key data &allow-other-keys)
+				    (setq reply data)))
+			:error (cl-function
+				(lambda (&key error-thrown &allow-other-keys)
+				  (setq err (string-trim (cdr error-thrown)))))
+			:sync t)
+    (when err
+      (error "Error communicating with AnkiConnect using cURL: %s" err))
+    (or reply (error "Got empty reply from AnkiConnect"))))
+
+(defun ol-anki-api-call-result (&rest args)
+  "Invoke AnkiConnect with ARGS and return the result from response.
+Raise an error if applicable."
+  (let-alist (apply #'ol-anki-api-call args)
+    (when .error (error .error))
+   .result
+    ))
 
 (provide 'ol-anki)
 ;;; ol-anki.el ends here
